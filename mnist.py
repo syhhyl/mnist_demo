47819d22c13bc4d2c67391a3df5778879fa91f6b
syhhyl <syhhyl926@126.com>
2025-10-21 16:49:14 +0800

    add model; acc=0.12359

--- mnist.py
+++ mnist.py
@@ -1,7 +1,10 @@
 import os
 from datasets import load_dataset, load_from_disk, Dataset
-import tinygrad
+from tinygrad import Device, Tensor, nn
 import numpy as np
+
+dev = Device.DEFAULT
+print(dev)
 
 mnist_dir = "mnist_datasets"
 # download and load mnist datasets from huggingface
@@ -18,4 +21,37 @@ train_labels = np.array(ds['train']['label'])
 test_images = np.array([np.array(img) for img in ds['test']['image']])
 test_labels = np.array(ds['test']['label'])
 
-print(train_images.shape)
+train_images = Tensor((train_images.astype(np.float32) / 255.0)[:, None, :, :], device=dev)
+train_labels = Tensor(train_labels.astype(np.int64), device=dev)
+test_images = Tensor((test_images.astype(np.float32) / 255.0)[:, None, :, :], device=dev, requires_grad=False)
+test_labels = Tensor(test_labels.astype(np.int64), device=dev)
+
+# print(train_images.shape)
+# print(train_labels.shape)
+
+
+class Model:
+  def __init__(self):
+    self.l1 = nn.Conv2d(1, 32, kernel_size=(3,3))
+    self.l2 = nn.Conv2d(32, 64, kernel_size=(3,3))
+    self.l3 = nn.Linear(1600, 10)
+    
+  def __call__(self, x: Tensor) -> Tensor:
+    x = self.l1(x).relu().max_pool2d((2, 2))
+    x = self.l2(x).relu().max_pool2d((2, 2))
+    return self.l3(x.flatten(1).dropout(0.5))
+    
+
+X_train, Y_train, X_test, Y_test = train_images, train_labels, test_images, test_labels
+# print(X_train.shape, X_train.dtype, Y_train.shape, Y_train.dtype)
+model = Model()
+acc = (model(X_test).argmax(axis=1) == Y_test).mean()
+print(acc.item())
+
+# for i in range(3):
+#   img = train_images[i, 0]
+#   for line in img:
+#     for e in line:
+#       print(e, end=' ')
+#     print()
+#   print("===============")
